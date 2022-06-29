47c50cfc0ddbf46defdb7f8895c87f6dd7e07cd3
makssyz <makss.golubs@gmail.com>
2022-06-29 12:11:10 +0200

    Add ML generated algorithm

--- include/leg.h
+++ include/leg.h
@@ -2,19 +2,19 @@
 
 /**
  * A single Leg which combines two servos into a single object. Information on the current angle and the initial
- * neutral angle for both the shin and thigh servo are saved, since it's faster and more reliable than reading
+ * neutral angle for both the knee and hip servo are saved, since it's faster and more reliable than reading
  * the actual angle from the physical servo.
  */
 class Leg {
 public:
-    Servo shin{};
-    Servo thigh{};
+    Servo knee{};
+    Servo hip{};
 
     char side{};
-    int current_shin_angle{};
-    int current_thigh_angle{};
-    int neutral_shin_angle{};
-    int neutral_thigh_angle{};
+    int current_knee_angle{};
+    int current_hip_angle{};
+    int neutral_knee_angle{};
+    int neutral_hip_angle{};
 
     /**
      * Initialize a single leg and connect the servo objects to the physical pins on the robot.
@@ -22,37 +22,37 @@ public:
      * For details on the Adeept Pixie's pins, refer to the robot's instructions.
      *
      * @param l_or_r: Is the leg on the left = 'l' or on the right = 'r' side of the robot according to its head.
-     * @param pin_thigh: Pin for the servo which is closer to the body was connected.
-     * @param pin_shin: Pin for the servo which controls the lower part of the leg that touches the ground.
-     * @param neutral_angle_thigh: The thigh's neutral angle, should be 45° from the body.
-     * @param neutral_angle_shin: The shin's neutral angle, should be 45° from the ground.
+     * @param pin_hip: Pin for the servo which is closer to the body was connected.
+     * @param pin_knee: Pin for the servo which controls the lower part of the leg that touches the ground.
+     * @param neutral_angle_hip: The hip's neutral angle, should be 45° from the body.
+     * @param neutral_angle_knee: The knee's neutral angle, should be 45° from the ground.
      */
-    void init(char l_or_r, int pin_thigh, int pin_shin, int neutral_angle_thigh, int neutral_angle_shin) {
+    void init(char l_or_r, int pin_hip, int pin_knee, int neutral_angle_hip, int neutral_angle_knee) {
         this->side = l_or_r;
 
-        this->current_thigh_angle = neutral_angle_thigh;
-        this->neutral_thigh_angle = neutral_angle_thigh;
-        this->neutral_shin_angle = neutral_angle_shin;
-        this->current_shin_angle = neutral_angle_shin;
+        this->current_hip_angle = neutral_angle_hip;
+        this->neutral_hip_angle = neutral_angle_hip;
+        this->neutral_knee_angle = neutral_angle_knee;
+        this->current_knee_angle = neutral_angle_knee;
 
-        this->thigh.attach(pin_thigh);
-        this->shin.attach(pin_shin);
+        this->hip.attach(pin_hip);
+        this->knee.attach(pin_knee);
     }
 
     /**
      * Moves a servo to an absolute angle while respecting the individual neutral angle of each servo.
      *
      * @param absolute_angle: Angle to move the servo.
-     * @param shin_or_thigh: Select the servo of a leg, e.g., shin = 's' or thigh = 't'.
+     * @param knee_or_hip: Select the servo of a leg, e.g., knee = 'k' or hip = 'h'.
      */
-    void write(int absolute_angle, char shin_or_thigh) {
-        switch (shin_or_thigh) {
-            case 't':
-                this->current_thigh_angle = absolute_angle - 90 + this->neutral_thigh_angle;
-                this->thigh.write(this->current_thigh_angle); break;
-            case 's':
-                this->current_shin_angle = absolute_angle - 90 + this->neutral_shin_angle;
-                this->shin.write(this->current_shin_angle);
+    void write(int absolute_angle, char knee_or_hip) {
+        switch (knee_or_hip) {
+            case 'h':
+                this->current_hip_angle = absolute_angle - 90 + this->neutral_hip_angle;
+                this->hip.write(this->current_hip_angle); break;
+            case 'k':
+                this->current_knee_angle = absolute_angle - 90 + this->neutral_knee_angle;
+                this->knee.write(this->current_knee_angle);
                 break;
             default:
                 return;
@@ -65,25 +65,25 @@ public:
         int start_thigh_angle;
         int start_shin_angle;
         if (from_neutral) {
-            start_thigh_angle = neutral_thigh_angle;
-            start_shin_angle = neutral_shin_angle;
+            start_thigh_angle = neutral_hip_angle;
+            start_shin_angle = neutral_knee_angle;
         } else {
-            start_thigh_angle = current_thigh_angle;
-            start_shin_angle = current_shin_angle;
+            start_thigh_angle = current_hip_angle;
+            start_shin_angle = current_knee_angle;
         }
         switch (direction) {
             case 'f':
-                this->current_thigh_angle = start_thigh_angle - degree;
-                this->thigh.write(current_thigh_angle); break;
+                this->current_hip_angle = start_thigh_angle - degree;
+                this->hip.write(current_hip_angle); break;
             case 'b':
-                this->current_thigh_angle = start_thigh_angle + degree;
-                this->thigh.write(this->current_thigh_angle); break;
+                this->current_hip_angle = start_thigh_angle + degree;
+                this->hip.write(this->current_hip_angle); break;
             case 'u':
-                this->current_shin_angle = start_shin_angle + degree;
-                this->shin.write(this->current_shin_angle); break;
+                this->current_knee_angle = start_shin_angle + degree;
+                this->knee.write(this->current_knee_angle); break;
             case 'd':
-                this->current_shin_angle = start_shin_angle - degree;
-                this->shin.write(this->current_shin_angle); break;
+                this->current_knee_angle = start_shin_angle - degree;
+                this->knee.write(this->current_knee_angle); break;
             default: return;
         }
     }
